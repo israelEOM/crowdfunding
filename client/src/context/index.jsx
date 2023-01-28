e4e0740e023037066851ff2fa80562fabbed0027
Israel Medeiros <israel.eom@gmail.com>
2023-01-28 23:12:00 +0000

    create campaign and home using contract

---
+++ client/src/context/index.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, createContext } from "react"
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react'
+import { ethers } from 'ethers'
+
+const StateContext = createContext()
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract('0xd897fBDF50b12bAf17A83667b155EE73C60dD8f4')
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign')
+  
+  const address = useAddress()
+  const connect = useMetamask()
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description, 
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image
+      ])
+
+      console.log('contract call success', data)
+    } catch (error) {
+      console.log('contract call failure', error)
+    }
+  }
+
+  const getCompaigns = async () => {
+    const campaigns = await contract.call('getCompaigns')
+
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }))
+
+    return parsedCampaigns
+  }
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCompaigns,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
-import react from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import react from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-import App from './App'
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App />
-        </Router>
-    </ThirdwebProvider>
-)
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
